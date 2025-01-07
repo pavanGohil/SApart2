02887e4dc08455ef16559c05ce21edd0e3823b0c
pavanGohil <158582735+pavanGohil@users.noreply.github.com>
2025-01-07 17:19:52 +0000

    Update ParcelMap.java

--- parcelMap.java
+++ parcelMap.java
@@ -1,2 +1,30 @@
-public class parcelMap {
+import java.util.HashMap;
+
+public class ParcelMap {
+    private HashMap<String, Parcel> parcelMap;
+
+    public ParcelMap() {
+        parcelMap = new HashMap<>();
+    }
+
+    public void addParcel(Parcel parcel) {
+        parcelMap.put(parcel.getParcelId(), parcel);
+    }
+
+    public Parcel getParcel(String parcelId) {
+        return parcelMap.get(parcelId);
+    }
+
+    public boolean containsParcel(String parcelId) {
+        return parcelMap.containsKey(parcelId);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Parcels in System:\n");
+        for (Parcel parcel : parcelMap.values()) {
+            sb.append(parcel).append("\n");
+        }
+        return sb.toString();
+    }
 }
