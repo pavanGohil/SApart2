2272049c7f7aa86b6bcc419f42adde8ece7de9df
pavanGohil <158582735+pavanGohil@users.noreply.github.com>
2025-01-07 17:17:41 +0000

    Update Log.java

--- log.java
+++ log.java
@@ -1,2 +1,38 @@
-public class log {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Log {
+    private static Log instance;
+    private StringBuilder log;
+
+    private Log() {
+        log = new StringBuilder();
+    }
+
+    public static Log getInstance() {
+        if (instance == null) {
+            instance = new Log();
+        }
+        return instance;
+    }
+
+    public void logEvent(String event) {
+        log.append(event).append("\n");
+    }
+
+    public void writeLogToFile(String filename) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(log.toString());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Test the Log class
+        Log log = Log.getInstance();
+        log.logEvent("Test event 1");
+        log.logEvent("Test event 2");
+        log.writeLogToFile("log.txt");
+        System.out.println("Log written to file.");
+    }
 }
