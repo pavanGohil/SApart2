58928b9734dc8e38eb56d03b46bbaca17454f25d
pavanGohil <158582735+pavanGohil@users.noreply.github.com>
2025-01-07 17:20:23 +0000

    Update QueofCustomers.java

--- queofCustomer.java
+++ queofCustomer.java
@@ -1,2 +1,30 @@
-public class queofCustomer {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueOfCustomers {
+    private Queue<Customer> customerQueue;
+
+    public QueOfCustomers() {
+        this.customerQueue = new LinkedList<>();
+    }
+
+    public void addCustomer(Customer customer) {
+        customerQueue.offer(customer);
+    }
+
+    public Customer removeCustomer() {
+        return customerQueue.poll();
+    }
+
+    public boolean isEmpty() {
+        return customerQueue.isEmpty();
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Customer customer : customerQueue) {
+            sb.append(customer.getName()).append("\n");
+        }
+        return sb.toString();
+    }
 }
