fc027aaa316dfc8bbca36415469d07ef16d12f38
pavanGohil <158582735+pavanGohil@users.noreply.github.com>
2025-01-07 17:20:46 +0000

    Update Worker.java

--- worker.java
+++ worker.java
@@ -1,2 +1,25 @@
-public class worker {
+public class Worker {
+    private Parcel currentParcel;
+
+    public void processCustomer(Customer customer, ParcelMap parcelMap) {
+        String parcelId = customer.getParcelId();
+
+        if (parcelMap.containsParcel(parcelId)) {
+            currentParcel = parcelMap.getParcel(parcelId);
+            System.out.println("Processing Customer: " + customer.getName());
+            System.out.println("Parcel Details: " + currentParcel);
+            System.out.printf("Fee Calculated: $%.2f%n", currentParcel.calculateFee());
+        } else {
+            System.out.println("Parcel not found for Customer: " + customer.getName());
+            currentParcel = null;
+        }
+    }
+
+    public String getCurrentParcelInfo() {
+        if (currentParcel != null) {
+            return currentParcel.toString();
+        } else {
+            return "No parcel currently being processed.";
+        }
+    }
 }
