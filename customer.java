37bd3c20582e1d1952ab785a347ad922a7e6f9f6
pavanGohil <158582735+pavanGohil@users.noreply.github.com>
2025-01-07 17:17:16 +0000

    Update Customer.java

--- customer.java
+++ customer.java
@@ -1,2 +1,28 @@
-public class customer {
+public class Customer {
+    private String name;
+    private String parcelId;
+
+    public Customer(String name, String parcelId) {
+        this.name = name;
+        this.parcelId = parcelId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getParcelId() {
+        return parcelId;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer Name: " + name + ", Parcel ID: " + parcelId;
+    }
+
+    public static void main(String[] args) {
+        // Test the Customer class
+        Customer customer = new Customer("Andrew Robertson", "X123");
+        System.out.println(customer);
+    }
 }
